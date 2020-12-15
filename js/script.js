ab60106252a31c64282ec02fa4ea03362e53792a
gabdigi <gabrieldigiart@gmail.com>
2020-12-15 23:01:12 +0100

    push

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let email = document.getElementById("email");
-let dangerIcon = document.getElementById('danger-button');
+let dangerIcon = document.getElementById("error");
 let dangerText = document.getElementById("danger-text");
 let form = document.getElementById("form");
 
@@ -13,35 +13,3 @@ form.addEventListener("submit", (e) => {
         dangerText.style.opacity = 0;
     }
 })
-
-/*const input = document.getElementById("email-input-box");
-const submitBtn = document.getElementById("submit-btn");
-
-function validateEmail(email) {
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-}
-
-function validate(email) {
-  if (!validateEmail(email)) {
-    const alertMsg = document.getElementById("alert-msg");
-    const alertFlag = document.getElementById("alert-flag");
-    alertMsg.classList.add("active");
-    alertFlag.classList.add("active");
-
-    setTimeout(() => {
-      alertMsg.classList.remove("active");
-      alertFlag.classList.remove("active");
-    }, 1000);
-  } else {
-    input.value = "";
-  }
-}
-
-submitBtn.addEventListener("click", (evt) => {
-  evt.preventDefault();
-
-  var email = input.value;
-  validate(email);
-});
-*/
